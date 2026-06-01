cfd317f86aab197b2b910bb0a6bc307e6e3415ed
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-06-01 15:44:05 +0700

    implement crash log saving

--- app/src/main/java/com/dertefter/etcetera/EtCetera.kt
+++ app/src/main/java/com/dertefter/etcetera/EtCetera.kt
@@ -1,9 +1,33 @@
 package com.dertefter.etcetera
 
 import android.app.Application
+import com.dertefter.data.repository.CrashlyticsRepository
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class EtCetera : Application() {
+
+    @Inject
+    lateinit var crashlyticsRepository: CrashlyticsRepository
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, exception ->
+            saveCrashLogToFile(exception)
+            defaultHandler?.uncaughtException(thread, exception)
+        }
+
+    }
+
+    private fun saveCrashLogToFile(exception: Throwable) {
+        try {
+            crashlyticsRepository.saveCrashLog(exception)
+        } catch (e: Exception) {
+        }
+    }
 
 }
