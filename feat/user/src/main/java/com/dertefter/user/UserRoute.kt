3a77f09018dd3dd7dfe402be90f3dea42ea6f035
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-06-22 19:50:22 +0700

    remove unused imports

--- feat/user/src/main/java/com/dertefter/user/UserRoute.kt
+++ feat/user/src/main/java/com/dertefter/user/UserRoute.kt
@@ -1,13 +1,10 @@
 package com.dertefter.user
 
-import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.dertefter.user.presentation.Event
 import com.dertefter.user.presentation.UserScreen
 
 @Composable
