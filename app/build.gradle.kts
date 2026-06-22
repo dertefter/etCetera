f989cb89dc57d1e9bd1a973138cc61520832fd71
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-06-22 20:11:17 +0700

    v0.0.9.1-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.dertefter.etcetera"
         minSdk = 29
         targetSdk = 37
-        versionCode = 9
-        versionName = "0.0.9-alpha"
+        versionCode = 10
+        versionName = "0.0.9.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
