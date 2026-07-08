912630b1fdecb409fa32e38470b92e8b041689fc
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-07-08 19:20:10 +0700

    move strings to res

--- app_mobile/design/src/main/java/com/dertefter/design/components/post/OriginalPostCard.kt
+++ app_mobile/design/src/main/java/com/dertefter/design/components/post/OriginalPostCard.kt
@@ -21,10 +21,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dertefter.design.R
 import com.dertefter.design.components.avatar.DisplayName
 import com.dertefter.design.components.avatar.SmallEmojiAvatar
 import com.dertefter.design.theme.AppTheme
@@ -54,7 +56,7 @@ fun OriginalPostCard(
     ) {
         if (originalPost.isDeleted) {
             Text(
-                text = "Пост был удален",
+                text = stringResource(R.string.design_post_deleted),
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.onSurfaceVariant,
                 modifier = Modifier
