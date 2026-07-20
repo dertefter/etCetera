924b7549e7aaefd6ec6dbd0b6081e9c580752534
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-07-20 13:44:38 +0700

    use EmojiAvatar in FollowerUserCard

--- app_mobile/feat/followers/src/main/java/com/dertefter/followers/presentation/component/FolloweruserCard.kt
+++ app_mobile/feat/followers/src/main/java/com/dertefter/followers/presentation/component/FolloweruserCard.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dertefter.data.dto.followers.FollowerUserDto
 import com.dertefter.design.components.avatar.DisplayName
-import com.dertefter.design.components.avatar.SmallEmojiAvatar
+import com.dertefter.design.components.avatar.EmojiAvatar
 import com.dertefter.design.theme.AppTheme
 import com.dertefter.design.theme.spacing
 import com.dertefter.followers.R
@@ -39,7 +39,7 @@ fun FollowerUserCard(
             .padding(horizontal = MaterialTheme.spacing.defaultScreenPadding, vertical = MaterialTheme.spacing.small),
         horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.large)
     ) {
-        SmallEmojiAvatar(
+        EmojiAvatar(
             emoji = followerUser.avatar,
             containerSize = 40.dp
         )
