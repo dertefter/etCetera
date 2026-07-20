268b1a6e64f40f2a71a4f83ce7f0ff820d9e93b8
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-07-20 13:37:27 +0700

    refactor avatar

--- app_mobile/design/src/main/java/com/dertefter/design/components/avatar/SmallEmojiAvatar.kt
+++ app_mobile/design/src/main/java/com/dertefter/design/components/avatar/SmallEmojiAvatar.kt
@@ -1,25 +1,26 @@
 package com.dertefter.design.components.avatar
 
-import android.content.res.Configuration
 import android.graphics.Canvas
 import android.graphics.Paint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.ToggleButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -27,7 +28,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -41,6 +45,9 @@ import androidx.graphics.shapes.star
 import androidx.palette.graphics.Palette
 import com.dertefter.design.components.common.RoundedPolygonShape
 import com.dertefter.design.theme.AppTheme
+import com.dertefter.design.theme.spacing
+import com.materialkolor.PaletteStyle
+import com.materialkolor.dynamiccolor.ColorSpec
 import com.materialkolor.ktx.harmonize
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -48,45 +55,65 @@ import kotlin.random.Random
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun SmallEmojiAvatar(
+fun EmojiAvatar(
     modifier: Modifier = Modifier,
     emoji: String,
-    containerSize: Dp = 48.dp,
+    containerSize: Dp = 52.dp,
     staticShape:  RoundedPolygon? = null,
+    strokeColor: Color = Color.Transparent,
+    strokeWidth: Dp = 2.dp,
     fontSize: TextUnit = 20.sp,
-    harmonizeColor: Color = MaterialTheme.colorScheme.surfaceContainerHighest,
+    rotation: Float = 0f,
     onClick: () -> Unit = {}
 ) {
 
+    val targetBgColor = MaterialTheme.colorScheme.primaryContainer
+    val targetShadowColor = MaterialTheme.colorScheme.onPrimaryFixed
     val fallbackColor = MaterialTheme.colorScheme.surfaceContainer
 
-    var detectedColor by remember { mutableStateOf(fallbackColor) }
+    var baseEmojiColor by remember(emoji) { mutableStateOf<Color?>(null) }
+
+    LaunchedEffect(emoji) {
+        baseEmojiColor = extractEmojiColor(
+            emoji = emoji,
+            defaultColorInt = Color.Transparent.toArgb()
+        )
+    }
+
+    val harmonizedBgColor = remember(baseEmojiColor, targetBgColor) {
+        val color = baseEmojiColor ?: return@remember fallbackColor
+        if (color == Color.Transparent) fallbackColor
+        else color.harmonize(targetBgColor, matchSaturation = true)
+    }
+
+    val harmonizedShadowColor = remember(baseEmojiColor, targetShadowColor) {
+        val color = baseEmojiColor ?: return@remember fallbackColor
+        if (color == Color.Transparent) fallbackColor
+        else color.harmonize(targetShadowColor, matchSaturation = true)
+    }
 
     val polygon = remember(emoji, staticShape) {
         staticShape ?: getEmojiPolygon(emoji)
     }
-    val clip = remember(polygon) {
-        RoundedPolygonShape(polygon = polygon)
-    }
 
-    LaunchedEffect(emoji, fallbackColor) {
-        val color = extractEmojiColor(
-            emoji = emoji,
-            defaultColorInt = fallbackColor.toArgb()
-        )
-        detectedColor = color
-            .harmonize(
-                other = harmonizeColor,
-                matchSaturation = true
-            )
+    val clip = remember(polygon, rotation) {
+        RoundedPolygonShape(polygon = polygon, rotation = rotation)
     }
 
     Box(
         modifier = modifier
+            .clickable(onClick = onClick)
+            .graphicsLayer {
+                this.shape = clip
+                this.clip = true
+            }
             .size(containerSize)
-            .clip(clip)
-            .background(detectedColor)
-            .clickable(onClick = onClick),
+            .border(
+                shape = clip,
+                color = strokeColor,
+                width = strokeWidth
+            )
+            .background(harmonizedBgColor),
         contentAlignment = Alignment.Center
     ) {
         Text(
@@ -94,7 +121,13 @@ fun SmallEmojiAvatar(
             maxLines = 1,
             fontSize = fontSize,
             modifier = Modifier,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                shadow = Shadow(
+                    color = harmonizedShadowColor.copy(alpha = 0.6f),
+                    blurRadius = 12f
+                )
+            )
         )
     }
 }
@@ -141,29 +174,65 @@ private suspend fun extractEmojiColor(
     Color(colorInt)
 }
 
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Preview
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
 @Composable
 fun PreviewAv() {
-    AppTheme {
+    var darkTheme by remember { mutableStateOf(true) }
+    var rotation by remember { mutableStateOf(0f) }
+    var currentEmojiIndex by remember { mutableIntStateOf(0) }
+
+    AppTheme(
+        seedColor = Color(0xFF5A1DC7).toArgb().toLong(),
+        darkTheme = darkTheme,
+        paletteStyle = PaletteStyle.Vibrant,
+        specVersion = ColorSpec.SpecVersion.Default
+    ) {
         val emojiList = listOf(
             "🌏", "🪲", "⚙️", "😍", "🖼️", "❤️", "😆", "🔥", "🌈", "🍎", "⚽", "🚗", "📱", "💻", "⌚",
             "🎧", "📷", "💡", "🔑", "🎁", "🎈", "🎉", "🎨", "🎭", "🎮", "🎲", "🎯", "🎳"
         )
 
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(4),
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        Column(
             modifier = Modifier
-                .clip(RoundedCornerShape(28.dp))
-                .background(MaterialTheme.colorScheme.surface)
-                .padding(2.dp)
-        ) {
-            items(emojiList) { emoji ->
-                SmallEmojiAvatar(emoji = emoji)
+                .clip(MaterialTheme.shapes.extraLarge)
+                .background(MaterialTheme.colorScheme.surfaceContainer)
+                .padding(MaterialTheme.spacing.extraLarge)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.large)
+        ){
+            EmojiAvatar(
+                emoji = emojiList[currentEmojiIndex],
+                rotation = rotation,
+                containerSize = 100.dp,
+                fontSize = 40.sp
+            )
+
+            Slider(
+                value = rotation,
+                onValueChange = { rotation = it },
+                valueRange = 0f..180f
+            )
+
+            Button(
+                onClick = {
+                    currentEmojiIndex = (currentEmojiIndex + 1) % emojiList.size
+                }
+            ) {
+                Text("Next emoji")
             }
+
+            ToggleButton(
+                checked = darkTheme,
+                onCheckedChange = { darkTheme = it }
+            ) {
+                Text(if (darkTheme) "Dark Theme" else "Light Theme")
+            }
+
         }
+
+
     }
 }
--- app_mobile/design/src/main/java/com/dertefter/design/components/common/RoundedPolygonShape.kt
+++ app_mobile/design/src/main/java/com/dertefter/design/components/common/RoundedPolygonShape.kt
@@ -1,5 +1,6 @@
 package com.dertefter.design.components.common
 
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Matrix
@@ -10,27 +11,29 @@ import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.graphics.shapes.RoundedPolygon
 import androidx.graphics.shapes.toPath
+import kotlin.math.max
 import kotlin.math.min
 
 fun RoundedPolygon.getBounds() = calculateBounds().let { Rect(it[0], it[1], it[2], it[3]) }
-
 class RoundedPolygonShape(
-    private val polygon: RoundedPolygon
+    private val polygon: RoundedPolygon,
+    private var matrix: Matrix = Matrix(),
+    private val rotation: Float = 0f
 ) : Shape {
-
+    private var path = Path()
     override fun createOutline(
         size: Size,
         layoutDirection: LayoutDirection,
         density: Density
     ): Outline {
-        val path = polygon.toPath().asComposePath()
-        val matrix = Matrix()
+        path.rewind()
+        path = polygon.toPath().asComposePath()
+        matrix.reset()
         val bounds = polygon.getBounds()
-        val scale = min(size.width / bounds.width, size.height / bounds.height)
-        matrix.translate(size.width / 2f, size.height / 2f)
-        matrix.scale(scale, scale)
-        matrix.translate(-(bounds.left + bounds.right) / 2f, -(bounds.top + bounds.bottom) / 2f)
-
+        val maxDimension = max(bounds.width, bounds.height)
+        matrix.scale(size.width / maxDimension, size.height / maxDimension)
+        matrix.translate(-bounds.left, -bounds.top)
+        matrix.rotateZ(rotation)
         path.transform(matrix)
         return Outline.Generic(path)
     }
