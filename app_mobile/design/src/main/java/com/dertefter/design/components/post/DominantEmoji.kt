d1b1af76756c46f03039f4235bf9193724a5c062
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-08-17 23:45:37 +0700

    add tooltip to DominantEmoji component

--- app_mobile/design/src/main/java/com/dertefter/design/components/post/DominantEmoji.kt
+++ app_mobile/design/src/main/java/com/dertefter/design/components/post/DominantEmoji.kt
@@ -8,21 +8,35 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
+import com.dertefter.design.R
 import com.dertefter.design.theme.AppTheme
 import com.dertefter.design.theme.spacing
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DominantEmoji(
     modifier: Modifier = Modifier,
@@ -36,30 +50,57 @@ fun DominantEmoji(
 
     val contentColor = MaterialTheme.colorScheme.onBackground
 
-    Row(
-        modifier = modifier
-            .clip(MaterialTheme.shapes.large)
-            .background(containerColor),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.small)
-    ){
+    val tooltipState = rememberTooltipState()
+    val scope = rememberCoroutineScope()
 
-        AnimatedContent(
-            targetState = dominantEmoji,
-            transitionSpec = {
-                slideInVertically(intOffsetSpec) { it } + fadeIn(floatSpec) togetherWith
-                        slideOutVertically(intOffsetSpec) { -it } + fadeOut(floatSpec) using
-                        SizeTransform(clip = false)
-            },
-            label = "dominantEmoji"
-        ) { emoji ->
-            Text(
-                text = emoji,
-                color = contentColor,
-                style = MaterialTheme.typography.bodyMedium
-            )
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+            positioning = TooltipAnchorPosition.Start
+        ),
+        tooltip = {
+            PlainTooltip(
+                containerColor = MaterialTheme.colorScheme.tertiary,
+                contentColor = MaterialTheme.colorScheme.onTertiary,
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Text(
+                    text = stringResource(R.string.design_post_captured_by_clan),
+                    style = MaterialTheme.typography.labelLargeEmphasized,
+                    modifier = Modifier.padding(MaterialTheme.spacing.small)
+                )
+
+            }
+        },
+        state = tooltipState
+    ) {
+        Row(
+            modifier = modifier
+                .clip(MaterialTheme.shapes.large)
+                .background(containerColor)
+                .clickable {
+                    scope.launch { tooltipState.show() }
+                },
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.small)
+        ){
+
+            AnimatedContent(
+                targetState = dominantEmoji,
+                transitionSpec = {
+                    slideInVertically(intOffsetSpec) { it } + fadeIn(floatSpec) togetherWith
+                            slideOutVertically(intOffsetSpec) { -it } + fadeOut(floatSpec) using
+                            SizeTransform(clip = false)
+                },
+                label = "dominantEmoji"
+            ) { emoji ->
+                Text(
+                    text = emoji,
+                    color = contentColor,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
         }
-
     }
 }
 
@@ -67,6 +108,11 @@ fun DominantEmoji(
 @Composable
 fun DominantEmojiPreview() {
     AppTheme {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ){
+
+        }
         DominantEmoji(
             dominantEmoji = "🙃"
         )
