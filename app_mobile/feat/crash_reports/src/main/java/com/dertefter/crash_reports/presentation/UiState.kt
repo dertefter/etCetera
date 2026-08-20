aaef409a12d336faae19b3199a3933bcad43e902
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-08-20 14:19:56 +0700

    upd crash_reposrts ui state

--- app_mobile/feat/crash_reports/src/main/java/com/dertefter/crash_reports/presentation/UiState.kt
+++ app_mobile/feat/crash_reports/src/main/java/com/dertefter/crash_reports/presentation/UiState.kt
@@ -1,9 +1,9 @@
 package com.dertefter.crash_reports.presentation
 
-import com.dertefter.data.dto.app.CrashlyticsItemDto
+import com.dertefter.data.dto.app.CrashlyticsItem
 
 data class UiState(
-    val reports: List<CrashlyticsItemDto> = emptyList(),
+    val reports: List<CrashlyticsItem> = emptyList(),
     val isLoading: Boolean = false,
     val selectedReportContent: String? = null
 )
