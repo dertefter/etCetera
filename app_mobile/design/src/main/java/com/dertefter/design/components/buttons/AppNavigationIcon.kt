9d3e8ed4b5c530cdb11722bafae7cf1811f43e40
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-08-21 14:01:42 +0700

    animate button width on pressed

--- app_mobile/design/src/main/java/com/dertefter/design/components/buttons/AppNavigationIcon.kt
+++ app_mobile/design/src/main/java/com/dertefter/design/components/buttons/AppNavigationIcon.kt
@@ -1,7 +1,10 @@
 package com.dertefter.design.components.buttons
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,15 +13,19 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dertefter.design.R
 import com.dertefter.design.icons.Icons
+import com.dertefter.design.theme.AppTheme
 import dev.chrisbanes.haze.HazeInput
 import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.blur.hazeBlur
@@ -36,16 +43,27 @@ fun AppNavigationIcon(
     hazeState: HazeState? = null
 ){
 
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val width by animateDpAsState(targetValue = if (isPressed) 64.dp else 40.dp, label = "width")
+
     val hazeStyle = HazeMaterials.ultraThin(
         containerColor = containerColor
     )
 
     Box(
         modifier = modifier
-            .padding(4.dp)
-            .size(40.dp)
+            .padding(all = 4.dp)
+            .size(
+                width = width,
+                height = 40.dp
+            )
             .clip(CircleShape)
-            .clickable(onClick = onClick),
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onClick
+            ),
         contentAlignment = Alignment.Center
     ){
 
@@ -69,7 +87,36 @@ fun AppNavigationIcon(
             imageVector = icon,
             tint = contentColor,
             contentDescription = contentDescription ?: stringResource(R.string.design_back_content_desc),
-            modifier = Modifier.size(24.dp)
+            modifier = Modifier
+                .size(24.dp)
         )
+    }
+}
+
+@Preview
+@Composable
+private fun AppNavigationIconPreview() {
+    AppTheme {
+        Box(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp)
+        ) {
+            AppNavigationIcon(onClick = {})
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AppNavigationIconDarkPreview() {
+    AppTheme(darkTheme = true) {
+        Box(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp)
+        ) {
+            AppNavigationIcon(onClick = {})
+        }
     }
 }
