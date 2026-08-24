92f1e2ebd18d8059ee7047019050c02fff67bc04
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-08-24 18:29:12 +0700

    v0.1.1-alpha

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,8 +29,8 @@ plugins {
     id("com.autonomousapps.build-health") version "3.18.0"
     
     // Ensure shared classloader for analysis plugin
-    id("com.android.application") version "9.3.1" apply false
-    id("com.android.library") version "9.3.1" apply false
+    id("com.android.application") version "9.3.2" apply false
+    id("com.android.library") version "9.3.2" apply false
     id("org.jetbrains.kotlin.android") version "2.4.10" apply false
 }
 dependencyResolutionManagement {
